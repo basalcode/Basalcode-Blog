d9617784b320ef389f90963c75ee83b228d83254
basalcode <basalcode@gmail.com>
2020-03-13 23:38:18 +0900

    fix : Modify improper size of main image
    About Changes…
    -major
    * Size of improper main image has been adjusted.
    
    -minor
    * Unnecessary codes were deleted.

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,17 @@
-var brightnessObj = {
-    setColor : function(color) {
-        document.querySelector('body').style.color = color;
-        var linkList = document.querySelectorAll('a');
-        for (var i = 0; i < linkList.length; i++) {
-            linkList[i].style.color = color;
-        }
-    },
-    setBackgroundColor : function(color) {
-        document.querySelector('body').style.backgroundColor = color;
-    }
-}
-
 function brightnessHandler(button) {
+    var brightnessObj = {
+        setColor : function(color) {
+            document.querySelector('body').style.color = color;
+            var linkList = document.querySelectorAll('a');
+            for (var i = 0; i < linkList.length; i++) {
+                linkList[i].style.color = color;
+            }
+        },
+        setBackgroundColor : function(color) {
+            document.querySelector('body').style.backgroundColor = color;
+        }
+    }
+
     if (button.value === "Dark Mode") {
         brightnessObj.setColor('white');
         brightnessObj.setBackgroundColor('black');
