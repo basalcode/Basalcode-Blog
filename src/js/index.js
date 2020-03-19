63f20407586c4313caddeccd74862aa8a88a9212
basalcode <basalcode@gmail.com>
2020-03-19 22:33:49 +0900

    add : Complete text animating function which works asynchronously
    About Changes…
    -major
    * Text writing animation was finally completed, and applied for all pages.
    
    -note
    * It needs to be refactored because of duplicated codes.

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,31 @@
-window.onload = function() {
+window.onload = async function() {
     var pElementList = document.querySelectorAll("p");
     var textList = [];
     textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem temporibus repellat magni vel possimus!");
     textList.push("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sapiente libero! At et non quibusdam.");
     textList.push("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium alias cupiditate vel, et praesentium.");
 
+    // Iterate every <p> element.
     for (var i = 0; i < pElementList.length; i++) {
         var text = textList.shift(); 
         var pTextField = document.createTextNode('');
         pElementList[i].appendChild(pTextField);
         var target = pElementList[i].firstChild;
         
+        // Iterate every word to write on the target, <p>.
         for (var j = 0; j < text.length; j++) {
-            window.setTimeout(
-                writeText(target, text[j]),
-                10000
-            );
+            await writeText(target, text[j], 50);
         }
     }
 
-    for (var i = 0; i < pElementList.length; i++) {
-        var textNode = document.createTextNode('');
-        writeText(textList.shift(), pElementList[i].appendChild(textNode));
+    // Actual writing function with an interval.
+    function writeText(target, text, ms) {
+        return new Promise(function(resolve) {
+            window.setTimeout(function() {
+                target.appendData(text);
+                resolve();
+            }, ms);
+        });
     }
 }
 
-function writeText(target, text) {
-    target.appendData(text);
-}
--- src/js/works.js
+++ src/js/works.js
@@ -1,19 +1,31 @@
-window.onload = function() {
-    var paragraphList = document.querySelectorAll("p");
-    var articleList = [];
-    articleList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
-    articleList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
-    articleList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
-    articleList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
+window.onload = async function() {
+    var pElementList = document.querySelectorAll("p");
+    var textList = [];
+    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
+    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
+    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
+    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste modi, vitae, dolor ut aliquam doloremque officia quisquam totam odit harum illum. Error aut itaque quos? Reiciendis aliquam esse ratione at.");
     
-    for (var i = 0; i < paragraphList.length; i++) {
-        var textNode = document.createTextNode('');
-        writeArticle(articleList.shift(), paragraphList[i].appendChild(textNode));
+    // Iterate every <p> element.
+    for (var i = 0; i < pElementList.length; i++) {
+        var text = textList.shift(); 
+        var pTextField = document.createTextNode('');
+        pElementList[i].appendChild(pTextField);
+        var target = pElementList[i].firstChild;
+        
+        // Iterate every word to write on the target, <p>.
+        for (var j = 0; j < text.length; j++) {
+            await writeText(target, text[j], 50);
+        }
     }
-}
 
-function writeArticle(text, target) {
-    for (var i = 0; i < text.length; i++) {
-        target.appendData(text[i]);
+    // Actual writing function with an interval.
+    function writeText(target, text, ms) {
+        return new Promise(function(resolve) {
+            window.setTimeout(function() {
+                target.appendData(text);
+                resolve();
+            }, ms);
+        });
     }
 }
