ee2b2e88bb1c49ba316c9a0e58f915dc18c29287
basalcode <basalcode@gmail.com>
2020-03-22 23:50:14 +0900

    add : Try to make caret animation effect
    About Changes…
    -major
    * Some codes to make caret animation were added, but did not work yet.

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,88 @@
 window.onload = function() {
-    var pElementList = document.querySelectorAll("p");
-    var textList = [];
-    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem temporibus repellat magni vel possimus!");
-    textList.push("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sapiente libero! At et non quibusdam.");
-    textList.push("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium alias cupiditate vel, et praesentium.");
+    function writeText() {
+        var textFiled = document.querySelectorAll(".txet-filed");
+        var textList = [];
+        textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem temporibus repellat magni vel possimus!");
+        textList.push("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sapiente libero! At et non quibusdam.");
+        textList.push("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium alias cupiditate vel, et praesentium.");
 
-    // Iterate every <p> element.
-    for (var i = 0; i < pElementList.length; i++) {
-        var text = textList.shift(); 
-        var pTextField = document.createTextNode('');
-        pElementList[i].appendChild(pTextField);
-        var target = pElementList[i].firstChild;
-        
-        wirteOnElement(target);
-    }
+        for (var i = 0; i < textFiled.length; i++) {
+            // About writing animation.
+            var text = textList.shift();
+            var pTextField = document.createTextNode('');
+            textFiled[i].appendChild(pTextField);
+            var writePos = textFiled[i].firstChild;    
+    
+            wirteOnElement(writePos, text);
+        }
 
-    // Take charge of animation for target element.
-    async function wirteOnElement(target) {
-        // Iterate every word to write on the target, <p>.
-        for (var j = 0; j < text.length; j++) {
-            if (text[j] ===' ' && Math.random() * 5 < 1) {
-                var waitOnSpace = Math.random() * 500 + 500;
-                await writeText(target, text[j], waitOnSpace);
-            }
-            else {
-                await writeText(target, text[j], 50);
+        // Take charge of animation for target element.
+        async function wirteOnElement(writePos, text) {
+            // Iterate every word to write on the target, <p>.
+            for (var j = 0; j < text.length; j++) {
+                if (text[j] ===' ' && Math.random() * 5 < 1) {
+                    var waitOnSpace = Math.random() * 500 + 500;
+                    await writeText(writePos, text[j], waitOnSpace);
+                }
+                else {
+                    await writeText(writePos, text[j], 50);
+                }
             }
         }
-    }
 
-    // Actual writing function with an interval.
-    function writeText(target, text, ms) {
-        return new Promise(function(resolve) {
-            window.setTimeout(function() {
-                target.appendData(text);
-                resolve();
-            }, ms); 
-        });
+        // Actual writing function with an interval.
+        function writeText(writePos, text, ms) {
+            return new Promise(function(resolve) {
+                window.setTimeout(function() {
+                    writePos.appendData(text);
+                    resolve();
+                }, ms); 
+            });
+        }
     }
+    
+
+    
+    // for (var i = 0; i < pElementList.length; i++) {
+    //     // About caret animation.
+    //     var newNode = document.createTextNode('');
+    //     pElementList[i].appendChild(newNode);
+    //     var caretPos = pElementList[i].lastChild;
+
+    //     // About writing animation.
+    //     var text = textList.shift();
+    //     var pTextField = document.createTextNode('');
+    //     pElementList[i].appendChild(pTextField);
+    //     var writePos = pElementList[i].firstChild;    
+
+    //     wirteOnElement(writePos, text);
+    //     wirteCaret(caretPos);
+    // }
+
+   
+
+    // async function wirteCaret(caretPos) {
+    //     caretPos.appendData('_');
+    //     while (true) {
+    //         await startCaret(caretPos);
+    //     }
+    // }
+
+    
+
+    // function startCaret(caretPos) {
+    //     return new Promise(function(resolve) {
+    //         window.setTimeout(function() {
+    //             if (caretPos[0] == '_') {
+    //                 console.log(caretPos[0]);
+    //                 caretPos.deleteData(0, 1);
+    //             } else {
+    //                 console.log(caretPos[0]);
+    //                 caretPos.appendData('_');
+    //             }
+    //             resolve();
+    //         }, 500);
+    //     });
+    // }
 }
 
