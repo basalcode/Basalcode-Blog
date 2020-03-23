ee576819079fc494b97c0b05ad6a80e94507f6dc
basalcode <basalcode@gmail.com>
2020-03-23 23:49:34 +0900

    add : Try to make caret animation effect 2
    About Changes…
    -major
    * Some codes to make caret animation were added, but did not work yet.
    
    -minor
    * Some typos were corrected.

--- src/js/index.js
+++ src/js/index.js
@@ -1,88 +1,72 @@
 window.onload = function() {
-    function writeText() {
-        var textFiled = document.querySelectorAll(".txet-filed");
-        var textList = [];
-        textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem temporibus repellat magni vel possimus!");
-        textList.push("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sapiente libero! At et non quibusdam.");
-        textList.push("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium alias cupiditate vel, et praesentium.");
-
-        for (var i = 0; i < textFiled.length; i++) {
-            // About writing animation.
-            var text = textList.shift();
-            var pTextField = document.createTextNode('');
-            textFiled[i].appendChild(pTextField);
-            var writePos = textFiled[i].firstChild;    
+    var textField = document.querySelectorAll(".text-field");
+    var caretField = document.querySelectorAll(".caret");
+    var textList = [];
+    textList.push("Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatem temporibus repellat magni vel possimus!");
+    textList.push("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, sapiente libero! At et non quibusdam.");
+    textList.push("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium alias cupiditate vel, et praesentium.");
     
-            wirteOnElement(writePos, text);
-        }
+    var writeOn = true;
+    for (var i = 0; i < textField.length; i++) {
+        // About caret animation.            
+        caretField[i].appendChild(document.createTextNode(''));
+        var caretPos = caretField[i].firstChild;
+        startCaret(caretPos);
 
-        // Take charge of animation for target element.
-        async function wirteOnElement(writePos, text) {
-            // Iterate every word to write on the target, <p>.
-            for (var j = 0; j < text.length; j++) {
-                if (text[j] ===' ' && Math.random() * 5 < 1) {
-                    var waitOnSpace = Math.random() * 500 + 500;
-                    await writeText(writePos, text[j], waitOnSpace);
-                }
-                else {
-                    await writeText(writePos, text[j], 50);
-                }
-            }
-        }
+        // About writing animation.
+        textField[i].appendChild(document.createTextNode(''));
+        var writePos = textField[i].firstChild;
+        wirteOnElement(writePos, textList.shift());
+    }
+    writeOn = false;
 
-        // Actual writing function with an interval.
-        function writeText(writePos, text, ms) {
-            return new Promise(function(resolve) {
-                window.setTimeout(function() {
-                    writePos.appendData(text);
-                    resolve();
-                }, ms); 
-            });
+    async function startCaret(caretPos) {
+        if (caretPos.length === 0) {
+            await writeCaret(caretPos).then(startCaret(caretPos));
+        } else {
+            await deleteCaret(caretPos).then(startCaret(caretPos));
         }
     }
-    
 
-    
-    // for (var i = 0; i < pElementList.length; i++) {
-    //     // About caret animation.
-    //     var newNode = document.createTextNode('');
-    //     pElementList[i].appendChild(newNode);
-    //     var caretPos = pElementList[i].lastChild;
+    function writeCaret(caretPos) {
+        return new Promise(function(resolve) {
+            window.setTimeout(function() {
+                caretPos.appendData('_');
+                resolve();
+            }, 500);
+        });
+    }
 
-    //     // About writing animation.
-    //     var text = textList.shift();
-    //     var pTextField = document.createTextNode('');
-    //     pElementList[i].appendChild(pTextField);
-    //     var writePos = pElementList[i].firstChild;    
+    function deleteCaret(caretPos) {
+        return new Promise(function(resolve) {
+            window.setTimeout(function() {
+                caretPos.deleteData(0, 1);
+                resolve();
+            }, 500);
+        });
+    }
 
-    //     wirteOnElement(writePos, text);
-    //     wirteCaret(caretPos);
-    // }
+    // Take charge of animation for target element.
+    async function wirteOnElement(writePos, text) {
+        // Iterate every word to write on the target, <p>.
+        for (var i = 0; i < text.length; i++) {
+            if (text[i] ===' ' && Math.random() * 5 < 1) {
+                var waitOnSpace = Math.random() * 500 + 500;
+                await writeText(writePos, text[i], waitOnSpace);
+            }
+            else {
+                await writeText(writePos, text[i], 50);
+            }
+        }
+    }
 
-   
-
-    // async function wirteCaret(caretPos) {
-    //     caretPos.appendData('_');
-    //     while (true) {
-    //         await startCaret(caretPos);
-    //     }
-    // }
-
-    
-
-    // function startCaret(caretPos) {
-    //     return new Promise(function(resolve) {
-    //         window.setTimeout(function() {
-    //             if (caretPos[0] == '_') {
-    //                 console.log(caretPos[0]);
-    //                 caretPos.deleteData(0, 1);
-    //             } else {
-    //                 console.log(caretPos[0]);
-    //                 caretPos.appendData('_');
-    //             }
-    //             resolve();
-    //         }, 500);
-    //     });
-    // }
+    // Actual writing function with an interval.
+    function writeText(writePos, text, ms) {
+        return new Promise(function(resolve) {
+            window.setTimeout(function() {
+                writePos.appendData(text);
+                resolve();
+            }, ms); 
+        });
+    }
 }
-
